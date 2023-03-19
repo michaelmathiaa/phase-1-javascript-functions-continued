5ecb3cc75a2089a31480d8d1b819f8636bdf425d
Michael Mathias <michaelscodingjourney@gmail.com>
2023-03-18 17:09:27 -0700

    Passed nine tests for javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function saturdayFun(saturdayActivity = 'roller-skate') {
+    return `This Saturday, I want to ${saturdayActivity}!`;
+}
+
+const mondayWork = function (mondayActivity = 'go to the office') {
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(outerP = '*') {
+    return result = function(innerP = 'special') {
+        return `You are ${outerP}${innerP}${outerP}!`;
+    }
+}
